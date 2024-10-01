4212a24b0e147878eb774afd4b6aa3527a8aef64
Segward <gustask@stud.ntnu.no>
2024-10-01 18:12:29 +0200

    Updated setup

--- setup.bat
+++ setup.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 
+echo --------------------------------
+
 rem Query the user-specific Path variable from the registry and store it in userPath
 for /f "tokens=2*" %%i in ('reg query "HKCU\Environment" /v Path 2^>nul') do (
     set "userPath=%%j"
@@ -11,7 +13,7 @@ SET "RELATIVE_BIN_PATH=.\bin"
 FOR %%i IN ("%RELATIVE_BIN_PATH%") DO SET "ABSOLUTE_BIN_PATH=%%~fi"
 
 rem Add a new path to the userPath variable
-set "unfilteredPath=C:\msys64\mingw64\bin;C:\msys64\mingw32\bin"
+set "unfilteredPath=;C:\msys64\mingw64\bin;C:\msys64\mingw32\bin"
 set "filteredPath="
 
 rem Add absoluate path to the unfilteredPath variable
@@ -22,11 +24,17 @@ for %%i in ("!unfilteredPath:;=";"!") do (
     set "isDuplicate=false"
     for %%j in ("!userPath:;=";"!") do (
         if "%%~i"=="%%~j" (
-            set "isDuplicate=true"
+            if not "%%~i"=="" (
+                echo %%~i is a duplicate
+                set "isDuplicate=true"
+            )
         )
     )
     if "!isDuplicate!"=="false" (
-        set "filteredPath=%filteredPath%;%%~i"
+        if not "%%~i"=="" (
+            echo %%~i is not a duplicate
+            set "filteredPath=!filteredPath!;%%~i"
+        )
     )
 )
 
@@ -35,6 +43,8 @@ set "userPath=%userPath%;%filteredPath%"
 
 rem Set the new userPath variable in the registry
 setx Path "%userPath%"
+
+echo -------------------------
 
 rem End the local environment changes
 endlocal
