29a41be0f5541a53cdc4d40cc5a460292828fa14
Segward <gustask@stud.ntnu.no>
2024-10-01 16:07:42 +0200

    Fix 1 Batch File

--- setup.bat
+++ setup.bat
@@ -1,49 +1,40 @@
 @echo off
-SETLOCAL
+setlocal enabledelayedexpansion
 
-REM Specify the relative path you want to add to the PATH
+:: Query the user-specific Path variable from the registry and store it in userPath
+for /f "tokens=2*" %%i in ('reg query "HKCU\Environment" /v Path 2^>nul') do (
+    set "userPath=%%j"
+)
+
+:: Get the absolute path of a relative path
 SET "RELATIVE_BIN_PATH=.\bin"
+FOR %%i IN ("%RELATIVE_BIN_PATH%") DO SET "ABSOLUTE_BIN_PATH=%%~fi"
 
-REM Get the absolute path from the relative path
-FOR %%I IN ("%RELATIVE_BIN_PATH%") DO SET "ABSOLUTE_BIN_PATH=%%~fI"
+:: Add a new path to the userPath variable
+set "unfilteredPath=C:\msys2\mingw64\bin;C:\msys2\mingw32\bin"
+set "filteredPath="
 
-REM Check if the directory is already in the PATH
-echo %PATH% | find /I "%ABSOLUTE_BIN_PATH%" >nul
+:: Add absoluate path to the unfilteredPath variable
+set "unfilteredPath=%unfilteredPath%;%ABSOLUTE_BIN_PATH%"
 
-IF %ERRORLEVEL% NEQ 0 (
-    REM If not, add it to the PATH
-    SETX PATH "%PATH%;%ABSOLUTE_BIN_PATH%"
-    echo Added %ABSOLUTE_BIN_PATH% to PATH.
-) ELSE (
-    echo %ABSOLUTE_BIN_PATH% is already in the PATH.
+:: Filter out paths that are already in the userPath variable
+for %%i in ("!unfilteredPath:;=";"!") do (
+    set "isDuplicate=false"
+    for %%j in ("!userPath:;=";"!") do (
+        if "%%~i"=="%%~j" (
+            set "isDuplicate=true"
+        )
+    )
+    if "!isDuplicate!"=="false" (
+        set "filteredPath=!filteredPath!;%%~i"
+    )
 )
 
-REM Get the absoluate path for the bin of mingw64 and mingw32
-SET "ABSOLUATE_MINGW64_BIN_PATH=C:\msys64\mingw64\bin"
-SET "ABSOLUATE_MINGW32_BIN_PATH=C:\msys64\mingw32\bin"
+:: Add the filtered path to the userPath variable
+set "userPath=%userPath%;%filteredPath%"
 
-REM Check if the directory is already in the PATH
-echo %PATH% | find /I "%ABSOLUATE_MINGW64_BIN_PATH%" >nul
+:: Set the new userPath variable in the registry
+setx Path "%userPath%"
 
-IF %ERRORLEVEL% NEQ 0 (
-    REM If not, add it to the PATH
-    SETX PATH "%PATH%;%ABSOLUATE_MINGW64_BIN_PATH%"
-    echo Added %ABSOLUATE_MINGW64_BIN_PATH% to PATH.
-) ELSE (
-    echo %ABSOLUATE_MINGW64_BIN_PATH% is already in the PATH.
-)
-
-REM Check if the directory is already in the PATH
-echo %PATH% | find /I "%ABSOLUATE_MINGW32_BIN_PATH%" >nul
-
-IF %ERRORLEVEL% NEQ 0 (
-    REM If not, add it to the PATH
-    SETX PATH "%PATH%;%ABSOLUATE_MINGW32_BIN_PATH%"
-    echo Added %ABSOLUATE_MINGW32_BIN_PATH% to PATH.
-) ELSE (
-    echo %ABSOLUATE_MINGW32_BIN_PATH% is already in the PATH.
-)
-
-echo.
-
-ENDLOCAL
+:: End the local environment changes
+endlocal
